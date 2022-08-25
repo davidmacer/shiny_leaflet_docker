2eabb2e174edc543db70766e682766cf87121326
David Martínez <jdavid.macer@gmail.com>
2022-08-25 14:14:46 -0500

    Agrega archivos para app de shiny

---
+++ server.R
@@ -0,0 +1,14 @@
+server <- function(input, output, session) {
+  
+  points <- eventReactive(input$recalc, {
+    cbind(rnorm(40) * 2 + 13, rnorm(40) + 48)
+  }, ignoreNULL = FALSE)
+  
+  output$mymap <- renderLeaflet({
+    leaflet() %>%
+      addProviderTiles(providers$Stamen.TonerLite,
+                       options = providerTileOptions(noWrap = TRUE)
+      ) %>%
+      addMarkers(data = points())
+  })
+}
